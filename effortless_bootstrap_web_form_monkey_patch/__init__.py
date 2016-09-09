d13a6aead3779b9394d348e91da6b59e98f7259a
Justin McGuire <jm@landedstar.com>
2016-09-09 13:45:35 -0400

    patch() is what we want

--- effortless_bootstrap_web_form_monkey_patch/__init__.py
+++ effortless_bootstrap_web_form_monkey_patch/__init__.py
@@ -1,2 +1,4 @@
-from effortless_bootstrap_web_form_monkey_patch import effortless_bootstrap_web_form_monkey_patch
+from effortless_bootstrap_web_form_monkey_patch import patch
+
+__all__ = [patch, ]
 
